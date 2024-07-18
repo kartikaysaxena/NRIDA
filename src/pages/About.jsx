da7af3fd322530ad9384e2d997d1be303b6356fb
devansh-rai <raianupam2002@gmail.com>
2024-07-19 00:00:42 +0530

    styling changes in About pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,27 @@
 export function About() {
     return (
-        <div className="items-center max-w-full px-96 pt-20">
-            <h1 className="text-4xl font-bold text-center">About NRDIA</h1>
-            <div className="p-10 font-normal text-xl max-w-7xl m-5">
-                <p>National Rural Infrastructure Development Agency (NRIDA) was registered as a society on 14th January, 2002 under the name ‘National Rural Roads Development Agency’ (NRRDA)with the basic objective of extending technical and management support to the Governmentof India in implementation of Pradhan Mantri Gram Sadak Yojana (PMGSY). It got its newname NRIDA in May 2017 after inclusion of rural housing activities in its scope of activities.
-                </p>
-                <br />
-                <p>NRIDA has been instrumental in setting high and uniform technical and management standards and facilitating policy development and planning at Central and State level in order to ensure sustainable management of the rural roads network under PMGSY. Till 9th July, 2024, 1,80,207 (including 100-249) habitations have been connected under the Ist Phase of PMGSY with a total road length of 4,22,731 km. of new connectivity besides upgradation of a total of 2,01,699 km. of road length. A further road length of 49,014 km has been upgraded till 9th July, 2024 under IInd Phase of PMGSY.
-                </p>
-                <br />
-                <p>Link to NRIDA
-                </p>
-                <br />
-                <a href="https://pmgsy.nic.in/" className="text-blue-500 hover:text-blue-300">https://pmgsy.nic.in/
-                </a>
+        <div className="items-center max-w-full p-5 pt-5 pb-10">
+            <div className="grid grid-cols-8 mt-5">
+                <div className="font-normal p-3 text-xl min-w-full col-span-3">
+                    <h1 className="text-4xl font-bold text-center mb-10">About NRDIA</h1>
+                    <p>National Rural Infrastructure Development Agency (NRIDA) was registered as a society on 14th January, 2002 under the name ‘National Rural Roads Development Agency’ (NRRDA)with the basic objective of extending technical and management support to the Governmentof India in implementation of Pradhan Mantri Gram Sadak Yojana (PMGSY). It got its newname NRIDA in May 2017 after inclusion of rural housing activities in its scope of activities.
+                    </p>
+                    <br />
+                    <p>NRIDA has been instrumental in setting high and uniform technical and management standards and facilitating policy development and planning at Central and State level in order to ensure sustainable management of the rural roads network under PMGSY. Till 9th July, 2024, 1,80,207 (including 100-249) habitations have been connected under the Ist Phase of PMGSY with a total road length of 4,22,731 km. of new connectivity besides upgradation of a total of 2,01,699 km. of road length. A further road length of 49,014 km has been upgraded till 9th July, 2024 under IInd Phase of PMGSY.
+                    </p>
+                    <br />
+                    <p>Link to NRIDA
+                    </p>
+                    <br />
+                    <a href="https://pmgsy.nic.in/" className="text-blue-500 hover:text-blue-300">https://pmgsy.nic.in/
+                    </a>
+                </div>
+                <div className="col-span-5 bg-red-200">
+                    <h1 className="text-center pt-72 font-bold">IMAGES</h1>
+                </div>
             </div>
+
+            
         </div>
 
     );
--- src/pages/AboutClg.jsx
+++ src/pages/AboutClg.jsx
@@ -1,19 +1,25 @@
 export function AboutClg()
 {
     return(
-        <div className="items-center max-w-full px-96 pt-20">
-            <h1 className="text-4xl font-bold text-center">About IIT Patna</h1>
-            <div className="p-10 font-normal text-xl max-w-7xl m-5">
-            <p>Indian Institute of Technology Patna is one of the new IITs established by an Act of the Indian Parliament on August 06, 2008.</p>
-                <br/>
-                <p>Patna which was known as Patliputra has been a center of knowledge since long has been attracting visitors and scholars from many parts of the world such as China, Indonesia, Japan, Korea, Sri Lanka, among others. This has been a land of visionaries. Some of the legends from this region include Lord Gautam Buddha, Lord Mahavir, Guru Gobind Singh, the famous astronomer Aryabhatta and the first President of India, Dr. Rajendra Prasad.</p>
-                <br/>
-                <p>IIT Patna has ten departments: These are Computer Science & Engineering, Electrical Engineering, Mechanical Engineering, Chemical and Biochemical Engineering, Civil & Environmental Engineering, Materials Science & Engineering, Chemistry, Physics, Mathematics and Humanities & Social Science departments.</p>
-                <br/>
-                <p>IIT Patna campus is located at Bihta which is approximately 40 kms from Patna. For driving directions and contact details, please point your browser to the following link:</p>
-                <br/><br/>
-                <a href="https://www.iitp.ac.in/index.php/en-us/contact?id=190" className="text-blue-500 hover:text-blue-300">https://www.iitp.ac.in/index.php/en-us/contact?id=190</a>      
-        </div>
+        <div className="items-center max-w-full p-5 pt-5 pb-10">
+            <div className="grid grid-cols-8 mt-5">
+                <div className="font-normal p-2 text-xl min-w-full col-span-3">
+                    <h1 className="text-4xl font-bold text-center mb-10">About IIT Patna</h1>
+                    <p>Indian Institute of Technology Patna is one of the new IITs established by an Act of the Indian Parliament on August 06, 2008.</p>
+                    <br/>
+                    <p>Patna which was known as Patliputra has been a center of knowledge since long has been attracting visitors and scholars from many parts of the world such as China, Indonesia, Japan, Korea, Sri Lanka, among others. This has been a land of visionaries. Some of the legends from this region include Lord Gautam Buddha, Lord Mahavir, Guru Gobind Singh, the famous astronomer Aryabhatta and the first President of India, Dr. Rajendra Prasad.</p>
+                    <br/>
+                    <p>IIT Patna has ten departments: These are Computer Science & Engineering, Electrical Engineering, Mechanical Engineering, Chemical and Biochemical Engineering, Civil & Environmental Engineering, Materials Science & Engineering, Chemistry, Physics, Mathematics and Humanities & Social Science departments.</p>
+                    <br/>
+                    <p>IIT Patna campus is located at Bihta which is approximately 40 kms from Patna. For driving directions and contact details, please point your browser to the following link:</p>
+                    <br/><br/>
+                    <a href="https://www.iitp.ac.in/index.php/en-us/contact?id=190" className="text-blue-500 hover:text-blue-300">https://www.iitp.ac.in/index.php/en-us/contact?id=190</a>      
+                </div>
+                <div className="bg-red-200 col-span-5">
+                    <h1 className="text-center pt-80 font-bold">IMAGES</h1>
+                </div>
+            </div>
+            
         </div>
         
     );
